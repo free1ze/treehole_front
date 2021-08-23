793fbad6595e68efe9ed386451d0b2b7ac35102f
SoullAngle <tzd772316639@163.com>
2021-08-23 10:50:45 +0800

    samestyle

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -11,7 +11,7 @@ Page({
     like: false,
     list: [{
         face_url: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=447979932,3108003765&fm=26&gp=0.jpg",
-        username: "哆啦B梦",
+        username: "#21",
         send_timestamp: "2019-7-6 14:42",
         centent: "阅读，是一次心灵的艺术之旅。前辈们留下了大量优秀的作品，通过这些传世之作给我们以启迪，教会我们如何感受世界。那些震撼心灵的好书往往意义深远，让人相逢恨晚。",
         total_likes: 2,
