38057fcff5a71be74c0536f1e783de1ee4c238bf
SoullAngle <tzd772316639@163.com>
2021-10-03 16:41:14 +0800

    modify verify page

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,5 @@
 // pages/login/login.js
+var interval = null //倒计时函数
 Page({
 
   /**
@@ -8,8 +9,37 @@ Page({
     is_disabled: false,
     username: "",
     password: "",
+    time: '获取验证码', //倒计时 
+    currentTime: 60
   },
+//控制倒计时
+  getCode: function (options){
+    var that = this;
+    var currentTime = that.data.currentTime
+    interval = setInterval(function () {
+      currentTime--;
+      that.setData({
+        time: currentTime+'秒'
+      })
+      if (currentTime <= 0) {
+        clearInterval(interval)
+        that.setData({
+          time: '重新发送',
+          currentTime:60,
+          disabled: false   
+        })
+      }
+    }, 1000)  
+  },
+  getVerificationCode: function(){
+    //这里加入调用发邮件的函数！！！
 
+    this.getCode();   //控制倒计时的函数
+    var that = this
+    that.setData({
+      disabled:true
+    })
+  },
   signup: function () {
     wx.navigateTo({
       url: '/pages/enroll/enroll'
